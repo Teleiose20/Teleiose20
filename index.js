a2deaf7c4ad3eb182870bd824dc049f547718b69
Igwe Justus <justusteleiose@gmail.com>
2023-10-20 23:22:55 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const handleShowSidebar = () => {
+  console.log(event.target);
+  const navlink = document.querySelector(".navlink");
+  navlink.classList.add("navlinkShown");
+};
+
+const handleRemoveSidebar = () => {
+  console.log(Event.target);
+  const navlink = document.querySelector(".navlink");
+  navlink.classList.add("navlinkShown");
+};
